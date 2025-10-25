d23f51e0818b913fa1f0099804419e1af2bc918d
Swayam Bhoir <swayambhoir579@gmail.com>
2025-10-25 18:41:13 +0530

    Prototype #1

--- frontend/src/componants/FileContainer/FileContainer.jsx
+++ frontend/src/componants/FileContainer/FileContainer.jsx
@@ -1,9 +1,29 @@
+import FileBox from "../FileBox/FileBox"
+
 import "./FileContainer.css"
 
+
 const FileContainer = () => {
+
+    const files = [
+        {
+            title: "Module 1: OSI Model",
+            lastUpdated: "20-10-2025"
+        },
+        {
+            title: "Module 2: Subnetting",
+            lastUpdated: "21-10-2025"
+        }
+    ]
+
     return (
         <div className="FileContainer">
-            this is file container
+            {
+                files.map(f => <FileBox
+                    title={f.title}
+                    lastUpdated={f.lastUpdated}
+                />)
+            }
         </div>
     )
 }
